023ffb6a386ef1675b47f43905d97035401babba
ConfusionPro <confurafael@gmail.com>
2025-02-02 10:23:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ function App() {
       {
         !valueSi ? (
           <div className="p-5">
-            <h1 className="text-white font-bold text-5x1 text-center">Ale</h1>
+            <h1 className="text-white font-bold text-7x1 text-center">Ale</h1>
             <h1 className="text-white font-bold text-5xl text-center">¿Quieres ser mi San Valentin?🥹❤️</h1>
             <img src={Object.keys(randomValor).length === 0 ?
               "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif" : randomValor.img} alt="San Valentin" className="mx-auto" width={400} height={400} />
